2185bd67859335f1723543a64ec86e1e4beaf1ea
NataliiaAndreieva <taliandreeva@gmail.com>
2023-09-29 18:59:17 +0300

    feedbackForm inProgres

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,35 @@
 import MainPage from 'pages/MainPage';
 import { Route, Routes } from 'react-router-dom';
 
+import { useState } from 'react';
+import { FeedbackForm } from 'components/FeedbackForm/FeedbackForm';
+
 export const App = () => {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+
     return (
         <Routes>
             <Route path="/" element={<MainPage />} />
             <Route path="/register" element={<div>Register</div>} />
             <Route path="/login" element={<div>Login</div>} />
+
+            <Route path="/" element={<MainPage />} />
+
+            {/* Додайте кнопку або елемент, який відкриватиме модальне вікно */}
+            <Route
+                path="/"
+                element={
+                    <button onClick={() => setIsModalOpen(true)}>
+                        Відкрити модальне вікно
+                    </button>
+                }
+            />
+
+            {/* Показуйте компонент FeedbackForm, якщо isModalOpen === true */}
+            {isModalOpen && (
+                <FeedbackForm onClose={() => setIsModalOpen(false)} />
+            )}
         </Routes>
     );
 };
--- src/redux/reviews/reviewsOperations.js
+++ src/redux/reviews/reviewsOperations.js
@@ -1,37 +1,94 @@
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// export const fetchReviews = createAsyncThunk(
+//     'reviews/fetchAll',
+//     async(_, thunkAPI) => {
+//     try {
+//         const { data } = await axios.get('reviews');
+//         return data.dara.reviews;
+//     } catch (e) {
+//         return thunkAPI.rejectWithValue(e.message);
+//     }
+//      }
+// );
+
+// export const fetchOwnReviews = createAsyncThunk(
+//     'reviews/fetchOwn',
+//     async (_, thunkAPI) => {
+//         try {
+//             const { data } = await axios.get('reviews/own');
+//             return data.data.reviews;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+
+//         }
+//     }
+// );
+
+// export const addReview = createAsyncThunk(
+//     'reviews/addReview',
+//     async (review, thunkAPI) => {
+//         try {
+//             const { data } = await axios.post('/reviews', review);
+//             return data.data;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+//         }
+//     }
+// );
+
+// export const deleteReview = createAsyncThunk(
+//     'reviews/deleteReviews',
+//     async (reviewId, thunkAPI) => {
+//         try {
+//             const { data } = await axios.delete(`/reviews/own/${reviewId}`);
+//             return data;
+//         } catch (e) {
+//             return thunkAPI.rejectWithValue(e.message);
+//         }
+//     }
+// );
+
+// export const updateReview = createAsyncThunk(
+//     'reviews/updateReviews',
+//     async ({ id, review }, thunkAPI) => {
+//     try {
+//         const { data } = await axios.patch(`/reviews/own/${id}`, review);
+//         return data.data;
+//     } catch (e) {
+//         return thunkAPI.rejectWithValue(e.message);
+//     }
+//     }
+// );
+
+
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const fetchReviews = createAsyncThunk(
-    'reviews/fetchAll',
-    async(_, thunkAPI) => {
-    try {
-        const { data } = await axios.get('reviews');
-        return data.dara.reviews;
-    } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
-    }
-     }
-);
+axios.defaults.baseURL = 'https://goose-track-backend-q3re.onrender.com/api-docs/#/';
 
-export const fetchOwnReviews = createAsyncThunk(
-    'reviews/fetchOwn',
-    async (_, thunkAPI) => {
+export const fetchReviews = createAsyncThunk(
+    'reviews/getReviews',
+    async ({ page, limit }, thunkAPI) => {
         try {
-            const { data } = await axios.get('reviews/own');
-            return data.data.reviews;
+            const { data } = await axios.get(
+                `/api/reviews?page=${page}&limit=4${limit}`
+            );
+
+            return data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
-
         }
     }
 );
 
 export const addReview = createAsyncThunk(
-    'reviews/addReview',
+    'reviews/addReviews',
     async (review, thunkAPI) => {
         try {
-            const { data } = await axios.post('/reviews', review);
-            return data.data;
+            const { data } = await axios.post('/api/reviews', review);
+            return data;
         } catch (e) {
             return thunkAPI.rejectWithValue(e.message);
         }
@@ -40,24 +97,38 @@ export const addReview = createAsyncThunk(
 
 export const deleteReview = createAsyncThunk(
     'reviews/deleteReviews',
-    async (reviewId, thunkAPI) => {
+    async (id, thunkAPI) => {
         try {
-            const { data } = await axios.delete(`/reviews/own/${reviewId}`);
+            const { data } = await axios.delete(`/api/reviews/${id}`);
+
             return data;
         } catch (e) {
-            return thunkAPI.rejectWithValue(e.message);
+            thunkAPI.rejectWithValue(e.message);
         }
     }
 );
 
-export const updateReview = createAsyncThunk(
-    'reviews/updateReviews',
+export const editReview = createAsyncThunk(
+    'reviews/editReviews',
     async ({ id, review }, thunkAPI) => {
-    try {
-        const { data } = await axios.patch(`/reviews/own/${id}`, review);
-        return data.data;
-    } catch (e) {
-        return thunkAPI.rejectWithValue(e.message);
+        try {
+            const { data } = await axios.patch(`/api/reviews/${id}`, review);
+
+            return data;
+        } catch (e) {
+            thunkAPI.rejectWithValue(e.message);
+        }
     }
+);
+
+export const fetchReviewById = createAsyncThunk(
+    'reviews/fetchReviewById',
+    async (id, thunkAPI) => {
+        try {
+            const { data } = await axios.get(`/api/reviews/${id}`);
+            return data;
+        } catch (e) {
+            thunkAPI.rejectWithValue(e.message);
+        }
     }
 );
