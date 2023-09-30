cbf71f5dc6b83523650480a9644e1776cbe0c9be
AntOnAntOff <povanton@gmail.com>
2023-10-01 01:10:52 +0300

    completes the AddFeedbackBtn component

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -39,11 +39,6 @@ export const MotivationContent = styled.div`
     }
 `;
 
-export const Img = styled.img`
-    width: 64px;
-    height: 60px;
-`;
-
 export const HeaderContainer = styled.div`
     display: flex;
     align-items: center;
@@ -107,35 +102,5 @@ export const MenuWrapperDiv = styled.div`
 
     @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
         gap: 24px;
-    }
-`;
-
-export const AddFeedbackBtn = styled.button`
-    padding: 0;
-    outline: none;
-    border: none;
-    cursor: pointer;
-    text-align: center;
-    font-family: Inter;
-    font-size: 12px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: calc(16 / 12);
-    padding: 12px 32px;
-    border-radius: 10px;
-    background-color: ${({ theme }) => theme.colors.backgroundBtn};
-    color: ${({ theme }) => theme.colors.white};
-
-    transition: ${({ theme }) =>
-        `background-color ${theme.animations.duration} ${theme.animations.cubicBezier}`};
-
-    @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
-        font-size: 14px;
-        line-height: calc(18 / 14);
-    }
-
-    :hover,
-    :focus {
-        background-color: ${({ theme }) => theme.colors.backgroundBtnHover};
     }
 `;
