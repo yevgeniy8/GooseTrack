0a187a2f951cf5082b4baf3c8ba97c88fe5546e4
Olena Spivakova <alena.spivakova1@gmail.com>
2023-10-02 22:33:09 +0200

    add review slider

--- src/components/ReviewsSlider/ReviewSlider.styled.jsx
+++ src/components/ReviewsSlider/ReviewSlider.styled.jsx
@@ -6,7 +6,7 @@ export const Container = styled.div`
     padding-right: 12px;
     margin: 0 auto;
     margin-top: 100px;
-    /* margin-bottom: 118px; */
+    margin-bottom: 118px;
     text-align: center;
 `;
 
