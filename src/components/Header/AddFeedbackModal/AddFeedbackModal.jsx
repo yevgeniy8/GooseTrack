9e4529e771f2e74cb198ee23fcfd1f687ff9397a
NataliiaAndreieva <taliandreeva@gmail.com>
2023-10-02 22:08:56 +0300

    some changes

--- src/components/Header/AddFeedbackModal/AddFeedbackModal.jsx
+++ src/components/Header/AddFeedbackModal/AddFeedbackModal.jsx
@@ -1,3 +1,10 @@
-// import { useState, useEffect } from 'react';
+import Modal from '../../Modal/Modal';
+import { FeedbackForm } from '../../FeedbackForm/FeedbackForm';
 
-export const AddFeedbackModal = ({})
+export const AddFeedbackModal = ({ onClose }) => {
+    return (
+        <Modal onClose={onClose}>
+            <FeedbackForm onClose={onClose} />
+        </Modal>
+    );
+};
