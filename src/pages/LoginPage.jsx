47d3c42ed704a4fc1a7dd6144383c2359bc90f7e
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-10-03 20:48:55 +0300

    comment tasks

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -34,12 +34,17 @@ const LoginPage = () => {
     );
 };
 
-const WrapperRegister = styled.body`
+const WrapperRegister = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: column;
     background-color: #dcebf7;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 `;
 
 const LinkToLogin = styled(NavLink)`
