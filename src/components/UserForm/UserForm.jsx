728071df75f4a95e7bc22dce97836f524ebf68d1
TetiPalii <TetianaPalii9491@gmail.com>
2023-10-04 15:23:52 +0200

    onsubmit function

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Field, Form, Formik } from 'formik';
+import { Formik } from 'formik';
 import * as yup from 'yup';
 import sprite from '../../images/icons.svg';
 
@@ -21,13 +21,15 @@ import {
 } from './UserForm.styled';
 import { ImgContainer } from './UserForm.styled';
 import { FieldsWrap } from './UserForm.styled';
-import { BirthdayDatePicker } from './ReactDatePickerCalendar';
 import { useSelector } from 'react-redux';
 import { selectUser } from 'redux/auth/authSelectors';
 import {
     DatePickWrapper,
     DatePickerStyled,
 } from './ReactDatePickerCalendar.styled';
+import moment from 'moment/moment';
+
+const dayMoment = moment().format('DD/MM/YYYY');
 
 // import { Calendar } from './Calendar';
 
@@ -41,53 +43,31 @@ const schema = yup.object().shape({
         .email()
         .matches(emailRegexp, 'email invalid')
         .required(),
-    birthday: yup.date(),
+    birthday: yup.date().max(dayMoment, 'Birthday must be earlier than today'),
     phone: yup.string().matches(phoneRegexp),
     skype: yup.string().max(16),
 });
 
 export const UserForm = () => {
     const [currentAvatar, setCurrentAvatar] = useState('');
-    const [currentPhone, setCurrentPhone] = useState('');
-    const [currentName, setCurrentName] = useState('');
-    const [currentEmail, setCurrentEmail] = useState('');
-    const [currentSkype, setCurrentSkype] = useState('');
-    const [currentBirthday, setCurrentBirthday] = useState('');
+    // const [currentPhone, setCurrentPhone] = useState('');
+    // const [currentName, setCurrentName] = useState('');
+    // const [currentEmail, setCurrentEmail] = useState('');
+    // const [currentSkype, setCurrentSkype] = useState('');
+    const [currentBirthday, setCurrentBirthday] = useState(new Date());
 
     const user = useSelector(selectUser);
     // console.log(user);
 
     const initialValues = {
-        name: currentName,
-        birthday: currentBirthday,
-        email: currentEmail,
-        phone: currentPhone,
-        skype: currentSkype,
+        name: '',
+        email: '',
+        phone: '',
+        skype: '',
     };
 
     const handleChange = ({ target }) => {
-        switch (target.name) {
-            case 'phone':
-                setCurrentPhone(target.value);
-                // console.log(currentPhone);
-                // console.log(target.value);
-                break;
-            case 'name':
-                setCurrentName(target.value);
-                break;
-            case 'email':
-                setCurrentEmail(target.value);
-                break;
-            case 'skype':
-                setCurrentSkype(target.value);
-                break;
-            case 'avatar':
-                setCurrentAvatar(target.files[0]);
-                break;
-
-            default:
-                break;
-        }
+        setCurrentAvatar(target.files[0]);
     };
 
     const handleSubmit = (values, actions) => {
@@ -110,7 +90,7 @@ export const UserForm = () => {
                 <SvgPlus width="14" height="14">
                     <use href={`${sprite}#icon-image`} />
                 </SvgPlus>
-                <UserName>{user.name || currentName}</UserName>
+                <UserName>{user.name || 'User Name'}</UserName>
                 <UserP>User</UserP>
             </AvatarContainer>
             <Formik
@@ -118,80 +98,94 @@ export const UserForm = () => {
                 validationSchema={schema}
                 onSubmit={handleSubmit}
             >
-                <StyledForm>
-                    <FieldsWrap>
-                        <LabelWrap>
-                            <Label htmlFor="">
-                                <Span>User name</Span>
-                                <InputForm
-                                    type="text"
-                                    name="name"
-                                    placeholder="Enter your name"
-                                    onChange={handleChange}
-                                    value={user.name || currentName}
-                                />
-                                <Error component="div" name="name" />
-                            </Label>
-                        </LabelWrap>
-                        <LabelWrap>
-                            <Label htmlFor="">
-                                <Span> Birthday</Span>
-                                <DatePickWrapper>
-                                    <DatePickerStyled
-                                        selected={currentBirthday}
-                                        onChange={date=>setCurrentBirthday(date)}
-                                        formatWeekDay={nameOfDay =>
-                                            nameOfDay.charAt(0)
-                                        }
-                                    />
-                                </DatePickWrapper>
-                                <Error component="div" name="birthday" />
-                            </Label>
-                        </LabelWrap>
+                {({
+              errors,
+              touched,
+              values
+             }) => {
+                    return (
+                        <StyledForm>
+                            <FieldsWrap>
+                                <LabelWrap>
+                                    <Label htmlFor="">
+                                        <Span>User name</Span>
+                                        <InputForm
+                                            type="text"
+                                            name="name"
+                                            placeholder="Enter your name"
+                                            // onChange={handleChange}
+                                            value={values.name}
+                                        />
+                                        <Error component="div" name="name" />
+                                    </Label>
+                                </LabelWrap>
+                                <LabelWrap>
+                                    <Label htmlFor="">
+                                        <Span> Birthday</Span>
+                                        <DatePickWrapper>
+                                            <DatePickerStyled
+                                                selected={currentBirthday}
+                                                onChange={date =>
+                                                    setCurrentBirthday(date)
+                                                }
+                                                formatWeekDay={nameOfDay =>
+                                                    nameOfDay.charAt(0)
+                                                }
+                                                name='birthday'
+                                            />
+                                        </DatePickWrapper>
+                                        <Error
+                                            component="div"
+                                            name="birthday"
+                                        />
+                                    </Label>
+                                </LabelWrap>
 
-                        <LabelWrap>
-                            <Label htmlFor="">
-                                <Span>Email</Span>
-                                <InputForm
-                                    type="email"
-                                    name="email"
-                                    placeholder="Enter email"
-                                    onChange={handleChange}
-                                    value={user.email || currentEmail}
-                                />
-                                <Error component="div" name="email" />
-                            </Label>
-                        </LabelWrap>
-                        <LabelWrap>
-                            <Label htmlFor="">
-                                <Span>Phone</Span>
-                                <InputForm
-                                    type="tel"
-                                    name="phone"
-                                    placeholder="Enter phone number"
-                                    onChange={handleChange}
-                                    value={currentPhone}
-                                />
-                                <Error component="div" name="phone" />
-                            </Label>
-                        </LabelWrap>
-                        <LabelWrap>
-                            <Label htmlFor="">
-                                <Span>Skype</Span>
-                                <InputForm
-                                    type="text"
-                                    name="skype"
-                                    placeholder="Add skype number"
-                                    value={currentSkype}
-                                    onChange={handleChange}
-                                />
-                                <Error component="div" name="skype" />
-                            </Label>
-                        </LabelWrap>
-                    </FieldsWrap>
+                                <LabelWrap>
+                                    <Label htmlFor="">
+                                        <Span>Email</Span>
+                                        <InputForm
+                                            type="email"
+                                            name="email"
+                                            placeholder="Enter email"
+                                            // onChange={handleChange}
+                                            value={values.email}
+                                        />
+                                        <Error component="div" name="email" />
+                                    </Label>
+                                </LabelWrap>
+                                <LabelWrap>
+                                    <Label htmlFor="">
+                                        <Span>Phone</Span>
+                                        <InputForm
+                                            type="tel"
+                                            name="phone"
+                                            placeholder="Enter phone number"
+                                            // onChange={handleChange}
+                                            value={values.phone}
+                                        />
+                                        <Error component="div" name="phone" />
+                                    </Label>
+                                </LabelWrap>
+                                <LabelWrap>
+                                    <Label htmlFor="">
+                                        <Span>Skype</Span>
+                                        <InputForm
+                                            type="text"
+                                            name="skype"
+                                            placeholder="Add skype number"
+                                            value={values.skype}
+                                            // onChange={handleChange}
+                                        />
+                                        <Error component="div" name="skype" />
+                                    </Label>
+                                </LabelWrap>
+                            </FieldsWrap>
 
-                    <Button type="submit">Save changes</Button>
-                </StyledForm>
+                            <Button type="submit">Save changes</Button>
+                        </StyledForm>
+                    );
+                }}
             </Formik>
         </MainContainer>
     );
