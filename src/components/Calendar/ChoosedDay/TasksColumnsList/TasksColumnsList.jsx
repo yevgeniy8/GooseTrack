e4054d91f2d55a5360a7e2c7aba86aa9a54f602d
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-10-04 21:54:03 +0300

    Update TasksColumnsList.jsx

--- src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
@@ -1,16 +1,16 @@
-import { ColumnList, Container } from './TasksColumnsList.styled';
+import { ColumnList } from './TasksColumnsList.styled';
 
 import TasksColumn from '../TasksColumn/TasksColumn';
 
 const TasksColumnsList = () => {
-  return (
-    // <Container>
-    <ColumnList>
-      <TasksColumn></TasksColumn>
-      <TasksColumn></TasksColumn>
-      <TasksColumn></TasksColumn>
-    </ColumnList>
-    // </Container>
-  );
+    return (
+        // <Container>
+        <ColumnList>
+            <TasksColumn></TasksColumn>
+            <TasksColumn></TasksColumn>
+            <TasksColumn></TasksColumn>
+        </ColumnList>
+        // </Container>
+    );
 };
 export default TasksColumnsList;
