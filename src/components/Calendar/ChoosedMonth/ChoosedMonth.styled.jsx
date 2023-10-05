24373a94aee82de9ae6346dde68090e6f9f977c8
Nata-Curly <nkucheryava@gmail.com>
2023-10-05 17:10:51 +0200

    some styles to calendar

--- src/components/Calendar/ChoosedMonth/ChoosedMonth.styled.jsx
+++ src/components/Calendar/ChoosedMonth/ChoosedMonth.styled.jsx
@@ -1,14 +1,5 @@
 import styled from '@emotion/styled';
 
-// export const WeekdaysBox = styled.div`
-// background-color: rgba(255, 255, 255, 1);
-// border: solid 1px rgba(220, 227, 229, 0.8);
-// border-radius: 8px;
-// `
-
-// export const CalendarContainer = styled.div`
-// position: absolute`
-
 export const WeekdaysList = styled.ul`
     display: flex;
     justify-content: space-around;
@@ -40,57 +31,53 @@ export const WeekdaysItem = styled.li`
 export const Table = styled.div`
     display: grid;
     grid-template-columns: repeat(7, 1fr);
-    ${props => props.fullheight && `height: calc(100% - 75px);`}
+    ${props => props.fullheight}
     ${props =>
         props.fullheight &&
         `grid-template-rows: repeat(${props.is28Days ? 4 : 5}, 1fr);`}
     div {
         display: grid;
-        max-width: 155px;
-        height: 125px;
+        justify-content: end;
+        // max-width: 155px;
+        height: 95px;
         background-color: white;
         border: 1px solid rgba(220, 227, 229, 0.8);
+            @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
+              max-width: 155px;
+              height: 125px;
+            };
         span {
             font-family: Inter;
-            font-size: 16px;
+            font-size: 12px;
             font-weight: 700;
-            line-height: 18px;
+            line-height: 14px;
             letter-spacing: 0em;
-            text-align: left;
-            text-align: right;
-            padding: 14px 14px;
+            text-align: center;
+            width: 22px;
+            height: 22px;
+            border-radius: 8px;
+            padding: 4px;
+            margin: 8px 4px 0 0;
             // height: fit-content;
             color: #343434;
+                @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
+                  margin: 14px 14px 0 0;
+                  font-size: 16px;
+                  font-weight: 700;
+                  line-height: 18px;
+                  width: 26px;
+                  height: 26px;
+    };
         }
 
         span.active {
-            background-color: #e3f3ff;
+            color: #FFF;
+            background-color: #3E85F3;
         }
-        span.active::before {
-            color: #3e85f3;
-            content: 'Today   ';
-            font-size: 16px;
-        }
+        // span.active::before {
+        //     color: #3e85f3;
+        //     content: 'Today   ';
+        //     font-size: 16px;
+        // }
     }
 `;
-
-// .item {
-//     width: calc((100% - 10 * 5px) / 5);
-//     margin: 5px;
-//     display: flex;
-//     flex-direction: column;
-//     justify-content: space-between;
-//     gap: 8px;
-//     padding: 5px;
-//     text-align: center;
-// }
-
-// .list {
-//     display: flex;
-//     flex-direction: row;
-//     flex-wrap: wrap;
-//     justify-content: center;
-//     gap: 15px;
-//     margin: -5px;
-//     transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
-// }
