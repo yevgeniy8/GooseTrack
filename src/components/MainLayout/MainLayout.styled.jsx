0a600a701aea7e201da26b09e4e750fdba57a01b
Тетяна Пономарчук <tanya061211@gmail.com>
2023-10-05 10:15:44 +0300

    fixed style: sideBar and MainLayout

--- src/components/MainLayout/MainLayout.styled.jsx
+++ src/components/MainLayout/MainLayout.styled.jsx
@@ -1,16 +1,34 @@
 import styled from '@emotion/styled';
 
 export const Wrapper = styled.div`
-    display: flex;
-    width: 100%;
+    
+background-color: ${({ theme }) => theme.colors.backgroundPrimary};
+display: flex;
+    width: 375px;
     min-height: 100vh;
     margin: 0 auto;
+    
+    
+    @media (min-width: 768px) {
+        width: 768px;
+    }
+
+    @media (min-width: 1440px) {
+        width: 1440px;
 `;
 
 export const Section = styled.div`
     width: 100%;
     min-height: 100%;
     display: flex;
+    // padding: 24px 20px 40px 20px;
     flex-direction: column;
-    background-color: ${({ theme }) => theme.colors.backgroundPrimar};
+
+    @media (min-width: 768px) {
+        // padding: 25px 32px 38px 32px;
+    }
+
+    @media (min-width: 1440px) {
+        // padding: 41px 32px 32px 32px;
+    }
 `;
