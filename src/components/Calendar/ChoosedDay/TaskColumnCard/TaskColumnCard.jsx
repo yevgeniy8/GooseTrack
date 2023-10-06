af02db13e7b3bee4237a9e5ca7bad0bfe27ca549
Тетяна Пономарчук <tanya061211@gmail.com>
2023-10-06 11:23:05 +0300

    done

--- src/components/Calendar/ChoosedDay/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/Calendar/ChoosedDay/TaskColumnCard/TaskColumnCard.jsx
@@ -1,32 +1,32 @@
 import {
-  CardItem,
-  CardContainer,
-  CardMain,
-  CardStatus,
-  CardTitle,
-  Container,
+    CardItem,
+    CardContainer,
+    CardMain,
+    CardStatus,
+    CardTitle,
+    Container,
 } from './TaskColumnCard.styled';
 import task from '../data/tasksdata.json';
 import TaskToolbar from './TaskToolbar';
 
 const TaskColumnCard = () => {
-  return (
-    <>
-      {task.map(taskData => (
-        <CardItem key={taskData.id}>
-          <CardContainer>
-            <CardTitle>{taskData.title}</CardTitle>
-            <CardMain>
-              <Container>
-                AVA <CardStatus>{taskData.priority}</CardStatus>
-              </Container>
-              <TaskToolbar></TaskToolbar>
-            </CardMain>
-          </CardContainer>
-        </CardItem>
-      ))}
-    </>
-  );
+    return (
+        <>
+            {task.map(taskData => (
+                <CardItem key={taskData.id}>
+                    <CardContainer>
+                        <CardTitle>{taskData.title}</CardTitle>
+                        <CardMain>
+                            <Container>
+                                AVA <CardStatus>{taskData.priority}</CardStatus>
+                            </Container>
+                            <TaskToolbar></TaskToolbar>
+                        </CardMain>
+                    </CardContainer>
+                </CardItem>
+            ))}
+        </>
+    );
 };
 
 export default TaskColumnCard;
--- src/components/Calendar/ChoosedDay/TaskColumnCard/TaskToolbar.jsx
+++ src/components/Calendar/ChoosedDay/TaskColumnCard/TaskToolbar.jsx
@@ -1,4 +1,6 @@
-import { CardEditMenu, CardEditBtn } from './TaskColumnCard.styled';
+import Icons from '../../../../images/icons.svg';
+import { Icon, CardEditMenu, CardEditBtn } from './TaskToolbar.styled';
+import {} from './TaskColumnCard.styled';
 // import task from '../../data/tasksdata.json';
 
 // const TaskColumnCard = () => {
@@ -13,28 +15,46 @@ import { CardEditMenu, CardEditBtn } from './TaskColumnCard.styled';
 // };
 
 const TaskToolbar = () => {
-  const handleAddClick = () => {
-    // Добавить обработчик для кнопки "Добавить"
-    alert('Вы нажали на кнопку "Добавить"');
-  };
+    const handleAddClick = () => {
+        // Добавить обработчик для кнопки "Добавить"
+        alert('Вы нажали на кнопку "Добавить"');
+    };
 
-  const handleEditClick = () => {
-    // Добавить обработчик для кнопки "Редактировать"
-    alert('Вы нажали на кнопку "Редактировать"');
-  };
+    const handleEditClick = () => {
+        // Добавить обработчик для кнопки "Редактировать"
+        alert('Вы нажали на кнопку "Редактировать"');
+    };
 
-  const handleDeleteClick = () => {
-    // Добавить обработчик для кнопки "Удалить"
-    alert('Вы нажали на кнопку "Удалить"');
-  };
-  return (
-    <>
-      <CardEditMenu>
-        <CardEditBtn onClick={handleAddClick}>Пер.</CardEditBtn>
-        <CardEditBtn onClick={handleEditClick}>Ред.</CardEditBtn>
-        <CardEditBtn onClick={handleDeleteClick}>Удал.</CardEditBtn>
-      </CardEditMenu>
-    </>
-  );
+    const handleDeleteClick = () => {
+        // Добавить обработчик для кнопки "Удалить"
+        alert('Вы нажали на кнопку "Удалить"');
+    };
+    return (
+        <>
+            <CardEditMenu>
+                <li>
+                    <CardEditBtn type="button" onClick={handleAddClick}>
+                        <Icon>
+                            <use href={`${Icons}#arrow-circle-broken-right`} />
+                        </Icon>
+                    </CardEditBtn>
+                </li>
+                <li>
+                    <CardEditBtn type="button" onClick={handleEditClick}>
+                        <Icon>
+                            <use href={`${Icons}#pencil`} />
+                        </Icon>
+                    </CardEditBtn>
+                </li>
+                <li>
+                    <CardEditBtn type="button" onClick={handleDeleteClick}>
+                        <Icon>
+                            <use href={`${Icons}#trash`} />
+                        </Icon>
+                    </CardEditBtn>
+                </li>
+            </CardEditMenu>
+        </>
+    );
 };
 export default TaskToolbar;
