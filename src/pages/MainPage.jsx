7cfab981679a7c3952aaed7e529346720d022fe0
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-10-06 21:14:54 +0300

    Update MainPage.jsx

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -5,17 +5,22 @@ import ReviewsSlider from 'components/ReviewsSlider/ReviewsSlider';
 import { Container } from 'components/Description/Description.styled';
 
 import React from 'react';
+import styled from '@emotion/styled';
 
 const MainPage = () => {
     return (
-        <div>
+        <Wrapper>
             <AuthSection />
             <Container>
                 <Description />
                 <ReviewsSlider />
             </Container>
-        </div>
+        </Wrapper>
     );
 };
+
+const Wrapper = styled.div`
+    background-color: white;
+`;
 
 export default MainPage;
