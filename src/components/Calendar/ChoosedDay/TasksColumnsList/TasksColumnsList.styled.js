a3a35ec2ba7ff6772473ba4f082afd7ee0a95ce6
Тетяна Пономарчук <tanya061211@gmail.com>
2023-10-07 17:12:59 +0300

    not finish

--- src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.js
+++ src/components/Calendar/ChoosedDay/TasksColumnsList/TasksColumnsList.styled.js
@@ -1,18 +1,19 @@
 import styled from '@emotion/styled';
 
 export const ColumnList = styled.ul`
-  display: flex;
-  justify-content: flex-start;
-  gap: 16px;
-  overflow: auto hidden;
-  scroll-snap-type: x mandatory;
+    display: flex;
+    justify-content: flex-start;
+    gap: 16px;
+    overflow: auto hidden;
+    scroll-snap-type: x mandatory;
 
-  &::-webkit-scrollbar {
-    width: 8px;
-  }
+    &::-webkit-scrollbar {
+        width: 8px;
+    }
 
-  &::-webkit-scrollbar-thumb {
-    border-radius: 12px;
-    background: #e7e5e5;
-  }
+    &::-webkit-scrollbar-thumb {
+        border-radius: 12px;
+
+        background: ${({ theme }) => theme.colors.scrollbarFix};
+    }
 `;
