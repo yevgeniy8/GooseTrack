87fc2c8ed8a16a1920634e918f32c7ac3c3514a9
Тетяна Пономарчук <tanya061211@gmail.com>
2023-10-08 21:09:49 +0300

    Overley done

--- src/components/MainLayout/MainLayout.jsx
+++ src/components/MainLayout/MainLayout.jsx
@@ -5,16 +5,24 @@ import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header';
 import SideBar from 'components/SideBar';
 import Spinner from 'components/Spinner/Spinner';
-import { Wrapper, Section } from './MainLayout.styled';
+import { Wrapper, Section, Overlay } from './MainLayout.styled';
 
 const MainLayout = () => {
     const [sideBareShow, setSideBareShow] = useState(window.innerWidth >= 1440);
+    const [isOverlayVisible, setIsOverlayVisible] = useState(false);
 
     const handleSideBareShow = () => {
         if (window.innerWidth >= 1440) {
             return;
         }
         setSideBareShow(!sideBareShow);
+        setIsOverlayVisible(!sideBareShow);
+
+        if (!sideBareShow) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = '';
+        }
     };
 
     useEffect(() => {
@@ -31,6 +39,7 @@ const MainLayout = () => {
 
     return (
         <Wrapper>
+            {isOverlayVisible && <Overlay />}
             <SideBar isOpen={sideBareShow} onCloseClick={handleSideBareShow} />
             {sideBareShow && window.innerWidth >= 1440 && (
                 <div id="smallScreenContainer" style={{ width: 289 }} />
--- src/components/MainLayout/MainLayout.styled.jsx
+++ src/components/MainLayout/MainLayout.styled.jsx
@@ -32,3 +32,13 @@ export const Section = styled.div`
         padding: 41px 32px 32px 32px;
     }
 `;
+
+export const Overlay = styled.div`
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 5;
+`;
