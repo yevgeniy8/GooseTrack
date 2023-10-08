4fffc3090b9927d6792fe95a8e0e95f3f23b422e
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-10-08 11:04:35 +0300

    knpk

--- src/components/Calendar/ChoosedDay/TasksColumn/ColumnHeadBar.jsx
+++ src/components/Calendar/ChoosedDay/TasksColumn/ColumnHeadBar.jsx
@@ -1,12 +1,14 @@
-import { ColumnHeader, ColumnContainer, PlusIcon } from './ColumnHeadBar.styled';
+import { ColumnHeader, ColumnContainer, PlusIcon, TopAddBTN } from './ColumnHeadBar.styled';
 import Icons from '../../../../images/icons.svg';
 const ColumnHeadBar = ({ header }) => {
   return (
     <ColumnContainer>
       <ColumnHeader>{header}</ColumnHeader>
-      <PlusIcon>
-        <use href={`${Icons}#icon-plus-add`} />
-      </PlusIcon>
+      <TopAddBTN>
+        <PlusIcon>
+          <use href={`${Icons}#icon-plus-add`} />
+        </PlusIcon>
+      </TopAddBTN>
     </ColumnContainer>
   );
 };
