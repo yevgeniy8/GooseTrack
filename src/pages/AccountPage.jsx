b39028a587903720eb48362c2432fdee6e1b7489
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-10-11 00:59:08 +0300

    commit

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,7 +1,24 @@
 import { UserForm } from 'components/UserForm/UserForm';
 
+import { createGlobalStyle } from 'styled-components';
+
 const AccountPage = () => {
-    return <UserForm />;
+    return (
+        <div>
+            <Global />
+            <UserForm />
+        </div>
+    );
 };
+
+const Global = createGlobalStyle`
+    body {
+        background-color: ${() => {
+            return localStorage.getItem('theme') === 'light'
+                ? '#F7F6F9'
+                : '#171820';
+        }};
+    }
+`;
 
 export default AccountPage;
