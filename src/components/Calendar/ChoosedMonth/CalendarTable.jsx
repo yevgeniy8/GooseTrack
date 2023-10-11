29c5cd84f3027884905fd661a3fc9c067367fa4d
Nata-Curly <nkucheryava@gmail.com>
2023-10-11 17:32:44 +0200

    task edit

--- src/components/Calendar/ChoosedMonth/CalendarTable.jsx
+++ src/components/Calendar/ChoosedMonth/CalendarTable.jsx
@@ -2,14 +2,13 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchTasks } from 'redux/calendar/calendarOperations';
 import { selectTasks } from 'redux/calendar/calendarSelector';
-// import styled from '@emotion/styled';
 
 import { datesAreOnSameDay, getDaysInMonth, getSortedDays } from './utils';
 import { Table } from './ChoosedMonth.styled';
 import { useNavigate } from 'react-router';
 import { useParams } from 'react-router';
 
-import { TaskModal } from '../../TaskModal/TaskModal';
+// import { TaskModal } from '../../TaskModal/TaskModal';
 import TaskList from './TaskList';
 import { NavLink } from 'react-router-dom';
 
@@ -26,22 +25,14 @@ const CalendarTable = () => {
         dispatch(fetchTasks(currentDate));
     }, [currentDate, dispatch]);
 
-    // const handleNavToDay = selectedDate => {
-    //     // const day = moment(selectedDate).format('YYYY-MM-DD');
-    //     // localStorage.getItem('day');
-    //     // // localStorage.setItem('type', 'day');
-    //     // localStorage.setItem('date', day);
-    //     navigate(`/calendar/day/${selectedDate}`);
-    //     // console.log(selectedDate); // Sun Oct 01 2023
-    //     // console.log(day); // 2023-10-01
+
+    // const [modalOpen, setModalOpen] = useState(false);
+
+    // const openModal = () => setModalOpen(true);
+    
+    // const closeModal = () => {
+    //     setModalOpen(false);
     // };
-
-    const [modalOpen, setModalOpen] = useState(false);
-
-    const openModal = () => setModalOpen(true);
-    const closeModal = () => {
-        setModalOpen(false);
-    };
 
     const handleClick = day => {
         if (day < 10) {
@@ -91,70 +82,17 @@ const CalendarTable = () => {
                             </span>
                         </NavLink>
                         <TaskList
-                            openModal={openModal}
+                            // openModal={openModal}
                             currentDate={currentDate}
                             day={day}
                             tasks={tasks}
                         />
-                        {/* <List>
-                        <Task>Low, very low priority</Task>
-                        <Task>Medium, medium priority</Task>
-                        <Task>High, very high priority</Task>
-                        </List> */}
                     </div>
                 ))}
             </Table>
-            {modalOpen && <TaskModal closeModal={closeModal} />}
+            {/* {modalOpen && <TaskModal closeModal={closeModal} />} */}
         </>
     );
 };
 
 export default CalendarTable;
-
-// const Task = styled.li`
-//     padding: 4 8px;
-//     border-radius: 8px;
-//     margin-bottom: 8px;
-//     max-width: 100%;
-//     color: #3E85F3;
-//     background-color: #CEEEFD;
-//     // background-color: ${props => props.bg};
-//     // color: ${props => props.color};
-//     font-family: Inter;
-//     font-size: 10px;
-//     font-weight: 700;
-//     line-height: 14px;
-//     letter-spacing: 0em;
-//     text-align: left;
-//     white-space: nowrap; /* Текст не переносится */
-//     overflow: hidden; /* Обрезаем всё за пределами блока */
-//     text-overflow: ellipsis; /* Добавляем многоточие */
-//      @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
-//         font-size: 14px;
-//         line-height: 18px;
-//   };
-//     `
-
-// const List = styled.ul`
-//     list-style: none;
-//     height: 65px;
-//     max-width: 44px;
-//     padding: 0 2px;
-//     margin: 0;
-//     overflow-y: auto;
-//      @media screen and (min-width: ${({ theme }) => theme.breakpoints.m}) {
-//         max-width: 100%;
-//         padding: 0 4px; };
-//     `
-
-//     // color={`color${task.priority}`}
-//     // bg={`bgcolor{task.priority}`}
-
-//     export const priorityColors = {
-//     colorLow: '#3E85F3',
-//     bgColorLow: '#CEEEFD',
-//     colorMedium: '#F3B249',
-//     bgColorMedium: '#FCF0D4',
-//     colorHgh: '#EA3D65',
-//     bgColorHigh: '#FFD2DD',
-// }
