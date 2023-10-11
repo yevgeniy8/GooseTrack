0231e91daa2ce0bf24c3c9688b4e6e3f0c8f10b7
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-10-11 23:40:15 +0300

    dAD

--- src/components/Calendar/CalendarToolbar/CalendarToolbar.jsx
+++ src/components/Calendar/CalendarToolbar/CalendarToolbar.jsx
@@ -2,124 +2,111 @@ import moment from 'moment';
 import sprite from 'images/icons.svg';
 
 import {
-    Date,
-    DateBox,
-    PeriodPaginator,
-    PeriodTypeSelect,
-    Icon,
-    ToolbarContainer,
-    BtnRight,
-    BtnLeft,
-    NavLinkMonth,
-    NavLinkDay,
+  Date,
+  DateBox,
+  PeriodPaginator,
+  PeriodTypeSelect,
+  Icon,
+  ToolbarContainer,
+  BtnRight,
+  BtnLeft,
+  NavLinkMonth,
+  NavLinkDay,
 } from './CalendarToolbar.styled';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 
 const CalendarToolbar = () => {
-    const [currentDate, setCurrentDate] = useState('');
-    const [format, setFormat] = useState('');
+  const [currentDate, setCurrentDate] = useState('');
+  const [format, setFormat] = useState('');
 
-    const month = moment(currentDate).format('YYYY-MM');
-    const day = moment(currentDate).format('YYYY-MM-DD');
+  const month = moment(currentDate).format('YYYY-MM');
+  const day = moment(currentDate).format('YYYY-MM-DD');
 
-    const date = useParams();
+  const date = useParams();
 
-    useEffect(() => {
-        if (date.currentDate) {
-            setFormat('month');
-            setCurrentDate(date.currentDate);
-        } else {
-            setFormat('day');
-            setCurrentDate(date.currentDay);
-        }
-    }, [date.currentDate, date.currentDay]);
+  useEffect(() => {
+    if (date.currentDate) {
+      setFormat('month');
+      setCurrentDate(date.currentDate);
+    } else {
+      setFormat('day');
+      setCurrentDate(date.currentDay);
+    }
+  }, [date.currentDate, date.currentDay]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handlePrevDate = format => {
-        const date = moment(currentDate)
-            .subtract(1, format)
-            .format('YYYY-MM-DD');
-        setCurrentDate(date);
-        localStorage.setItem('date', date);
-        if (format === 'month') {
-            const dataData = moment(date).format('YYYY-MM');
-            navigate(`${format}/${dataData}`);
-        } else {
-            navigate(`${format}/${date}`);
-        }
-    };
+  const handlePrevDate = format => {
+    const date = moment(currentDate).subtract(1, format).format('YYYY-MM-DD');
+    setCurrentDate(date);
+    localStorage.setItem('date', date);
+    if (format === 'month') {
+      const dataData = moment(date).format('YYYY-MM');
+      navigate(`${format}/${dataData}`);
+    } else {
+      navigate(`${format}/${date}`);
+    }
+  };
 
-    const handleNextDate = format => {
-        const date = moment(currentDate).add(1, format).format('YYYY-MM-DD');
-        setCurrentDate(date);
-        localStorage.setItem('date', date);
-        if (format === 'month') {
-            const dataData = moment(date).format('YYYY-MM');
-            navigate(`${format}/${dataData}`);
-        } else {
-            navigate(`${format}/${date}`);
-        }
-    };
+  const handleNextDate = format => {
+    const date = moment(currentDate).add(1, format).format('YYYY-MM-DD');
+    setCurrentDate(date);
+    localStorage.setItem('date', date);
+    if (format === 'month') {
+      const dataData = moment(date).format('YYYY-MM');
+      navigate(`${format}/${dataData}`);
+    } else {
+      navigate(`${format}/${date}`);
+    }
+  };
 
+  const handleChangeType = e => {
+    switch (e.currentTarget.textContent) {
+      case 'Month':
+        setFormat('month');
+        localStorage.setItem('type', 'month');
+        navigate(`month/${month}`);
+        break;
+      case 'Day':
+        setFormat('day');
+        localStorage.setItem('type', 'day');
+        navigate(`day/${localStorage.getItem('date')}`);
+        break;
+      default:
+        return format;
+    }
+  };
 
-    const handleChangeType = e => {
-        switch (e.currentTarget.textContent) {
-            case 'Month':
-                setFormat('month');
-                localStorage.setItem('type', 'month');
-                navigate(`month/${month}`);
-                break;
-            case 'Day':
-                setFormat('day');
-                localStorage.setItem('type', 'day');
-                navigate(`day/${localStorage.getItem('date')}`);
-                break;
-            default:
-                return format;
-        }
-    };
-
-    return (
-        <ToolbarContainer>
-            <PeriodTypeSelect>
-                <DateBox>
-                    <Date>
-                        {moment(currentDate).format(
-                            format === 'day' ? 'DD MMM YYYY' : 'MMMM YYYY'
-                        )}
-                    </Date>
-                </DateBox>
-                <div>
-                    <BtnLeft
-                        type="button"
-                        onClick={() => handlePrevDate(format)}
-                    >
-                        <Icon width="18" height="18">
-                            <use href={`${sprite}#chevron-left`} />
-                        </Icon>
-                    </BtnLeft>
-                    <BtnRight
-                        type="button"
-                        onClick={() => handleNextDate(format)}
-                    >
-                        <Icon width="18" height="18">
-                            <use href={`${sprite}#chevron-right`} />
-                        </Icon>
-                    </BtnRight>
-                </div>
-            </PeriodTypeSelect>
-            <PeriodPaginator>
-                <NavLinkMonth to={`month/${month}`} onClick={handleChangeType}>
-                    Month
-                </NavLinkMonth>
-                <NavLinkDay to={`day/${day}`} onClick={handleChangeType}>
-                    Day
-                </NavLinkDay>
-            </PeriodPaginator>
-        </ToolbarContainer>
-    );
+  return (
+    <ToolbarContainer>
+      <PeriodTypeSelect>
+        <DateBox>
+          <Date>{moment(currentDate).format(format === 'day' ? 'DD MMM YYYY' : 'MMMM YYYY')}</Date>
+        </DateBox>
+        <div>
+          <BtnLeft type="button" aria-label="previous-date" onClick={() => handlePrevDate(format)}>
+            <Icon width="18" height="18">
+              <use href={`${sprite}#chevron-left`} />
+            </Icon>
+          </BtnLeft>
+          <BtnRight type="button" aria-label="next-date" onClick={() => handleNextDate(format)}>
+            <Icon width="18" height="18">
+              <use href={`${sprite}#chevron-right`} />
+            </Icon>
+          </BtnRight>
+        </div>
+      </PeriodTypeSelect>
+      <PeriodPaginator>
+        <NavLinkMonth to={`month/${month}`} onClick={handleChangeType}>
+          Month
+        </NavLinkMonth>
+        <NavLinkDay to={`day/${day}`} onClick={handleChangeType}>
+          Day
+        </NavLinkDay>
+      </PeriodPaginator>
+    </ToolbarContainer>
+  );
 };
 
 export default CalendarToolbar;
