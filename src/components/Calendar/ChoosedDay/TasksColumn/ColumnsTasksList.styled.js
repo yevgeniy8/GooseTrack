c564178d809fb11c852285a79b08b3f2ff4c1d6a
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-10-12 02:36:34 +0300

    выфвыфвы

--- src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
+++ src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
@@ -1,35 +1,47 @@
 import styled from '@emotion/styled';
-
+import { tablet, mobile, desktop, min } from '../../../../styles/media';
 export const TaskList = styled.ul`
-    gap: 17px;
-    display: flex;
-    flex-direction: column;
-    padding: 0px;
+  gap: 17px;
+  display: flex;
+  flex-direction: column;
+  padding: 0px;
 
-    overflow: auto hidden;
-    overflow-y: auto;
-    padding-right: 7px;
-    position: relative;
-    margin-top: 24px;
+  overflow: auto hidden;
+  overflow-y: auto;
+  padding-right: 7px;
+  position: relative;
+  margin-top: 24px;
+  height: 300px;
 
-    &::-webkit-scrollbar {
-        width: 8px;
-        padding: 50px;
-    }
+  &::-webkit-scrollbar {
+    width: 8px;
+    padding: 50px;
+  }
 
-    &::-webkit-scrollbar-thumb {
-        padding: 50px;
-        border-radius: 12px;
-        background: ${({ theme }) => theme.colors.scrollbar};
-    }
+  &::-webkit-scrollbar-thumb {
+    padding: 50px;
+    border-radius: 12px;
+    background: ${({ theme }) => theme.colors.scrollbar};
+  }
 
-    &::-webkit-scrollbar-track {
-        background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
-    }
+  &::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
+  }
+  ${min(mobile)} {
+    ${'' /* max-height: 432px; */}
+    height: 260px;
+  }
+  ${min(tablet)} {
+    height: 379;
+  }
+
+  ${min(desktop)} {
+    height: 380px;
+  }
 `;
 export const ColumnList = styled.ul`
-    display: flex;
-    gap: 27px;
+  display: flex;
+  gap: 27px;
 `;
 export const ColumnItem = styled.li`
     flex-basis: calc((100% - 2 * 27px) / 3);
