f2232ee759cdce4fa6cbfcc3a77f7a876badab12
Тетяна Пономарчук <tanya061211@gmail.com>
2023-10-12 17:54:28 +0300

    fix

--- src/components/Calendar/ChoosedDay/TaskColumnCard/TaskColumnCard.styled.jsx
+++ src/components/Calendar/ChoosedDay/TaskColumnCard/TaskColumnCard.styled.jsx
@@ -1,82 +1,92 @@
 import styled from '@emotion/styled';
 
 export const CardItem = styled.li`
-  padding: 14px 14px 18px 14px;
-  border-radius: 8px;
+    padding: 14px 14px 18px 14px;
+    border-radius: 8px;
 
-  word-wrap: break-word;
+    word-wrap: break-word;
 
-  background: ${({ theme }) => theme.colors.backgroundPrimary};
-  border: ${({ theme }) => theme.borders.calendar};
+    background: ${({ theme }) => theme.colors.backgroundPrimary};
+    border: ${({ theme }) => theme.borders.calendar};
 `;
 export const CardContainer = styled.div`
-  width: 100%;
+    width: 100%;
 `;
 
 export const CardMain = styled.div`
-  display: flex;
+    display: flex;
 
-  justify-content: space-between;
+    justify-content: space-between;
 
-  align-items: flex-end;
+    align-items: flex-end;
 `;
 export const CardTitle = styled.h3`
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 1.28;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.28;
 
-  padding: 0px;
-  margin-bottom: 30px;
+    padding: 0px;
+    margin-bottom: 30px;
 
-  color: ${({ theme }) => theme.colors.textPrimary};
+    color: ${({ theme }) => theme.colors.textPrimary};
 `;
 
 export const Container = styled.div`
-  display: flex;
-  align-items: flex-end;
-
-  gap: 8px;
-  height: 32px;
-  width: 32px;
-
-  border-radius: 50%;
-  border: 1.8px solid #3e85f3;
+    display: flex;
+    align-items: flex-end;
 `;
 
 export const StatusHigh = styled.p`
-  border-radius: 4px;
+    border-radius: 4px;
 
-  text-align: center;
-  font-size: 10px;
-  line-height: 1.2;
+    text-align: center;
+    font-size: 10px;
+    line-height: 1.2;
 
-  max-height: 20px;
-  padding: 4px 12px;
+    max-height: 20px;
+    padding: 4px 12px;
 
-  color: white;
-  background-color: #ea3d65;
+    color: white;
+    background-color: #ea3d65;
 `;
 export const StatusLow = styled.p`
-  border-radius: 4px;
-  font-size: 10px;
-  line-height: 1.2;
+    border-radius: 4px;
+    font-size: 10px;
+    line-height: 1.2;
 
-  max-height: 20px;
-  padding: 4px 12px;
+    max-height: 20px;
+    padding: 4px 12px;
 
-  color: white;
-  background-color: #72c2f8;
+    color: white;
+    background-color: #72c2f8;
 `;
 
 export const StatusMedium = styled.p`
-  border-radius: 4px;
-  font-size: 10px;
-  line-height: 1.2;
+    border-radius: 4px;
+    font-size: 10px;
+    line-height: 1.2;
 
-  max-height: 20px;
-  padding: 4px 12px;
+    max-height: 20px;
+    padding: 4px 12px;
 
-  color: white;
-  background-color: #f3b249;
+    color: white;
+    background-color: #f3b249;
+`;
+
+export const AvatarWrap = styled.div`
+    display: flex;
+    justify-content: center;
+
+    gap: 8px;
+    height: 32px;
+    width: 32px;
+    margin-right: 8px;
+    border-radius: 50%;
+    border: 1.8px solid #3e85f3;
+`;
+
+export const ConteinerAvatar = styled.div`
+    display: flex;
+    justify-content: center;
 `;
