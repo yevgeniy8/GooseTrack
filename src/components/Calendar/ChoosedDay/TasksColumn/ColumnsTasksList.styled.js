a982e8842ae4cb121f0d38dfaf2f17e4db4fb60c
Bohdan Shevaniuk <s.bogdan1555@gmail.com>
2023-10-12 14:09:00 +0300

    ewsgresdgy

--- src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
+++ src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
@@ -2,54 +2,52 @@ import styled from '@emotion/styled';
 import { tablet, mobile, desktop, min } from '../../../../styles/media';
 
 export const TaskList = styled.ul`
-    display: flex;
-    flex-direction: column;
-    position: relative;
-    gap: 17px;
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  gap: 17px;
 
-    overflow: hidden auto;
-    /* overflow-y: auto; */
+  max-height: 255px;
+  padding-right: 7px;
+  margin-top: 24px;
 
-    padding-right: 7px;
-    margin-top: 24px;
-    /* height: 120px; */
-    margin-bottom: 22px;
-    max-height: 378px;
+  margin-bottom: 22px;
+  /* overflow: hidden auto; */
+  overflow-y: auto;
+  &::-webkit-scrollbar {
+    width: 8px;
+    padding: 50px;
+  }
 
-    &::-webkit-scrollbar {
-        width: 8px;
-        padding: 50px;
-    }
+  &::-webkit-scrollbar-thumb {
+    padding: 50px;
+    border-radius: 12px;
+    background: ${({ theme }) => theme.colors.scrollbar};
+  }
 
-    &::-webkit-scrollbar-thumb {
-        padding: 50px;
-        border-radius: 12px;
-        background: ${({ theme }) => theme.colors.scrollbar};
-    }
-
-    &::-webkit-scrollbar-track {
-        background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
-    }
-    /* ${min(mobile)} {
-    height: 120px;
+  &::-webkit-scrollbar-track {
+    background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
+  }
+  ${min(mobile)} {
+    max-height: 255px;
   }
   ${min(tablet)} {
-    height: 250px;
+    max-height: 324px;
   }
 
   ${min(desktop)} {
-    height: 380px;
-  } */
+    max-height: 378px;
+  }
 `;
 
 export const ColumnList = styled.ul`
-    display: flex;
-    gap: 27px;
+  display: flex;
+  gap: 27px;
 `;
 
 export const ColumnItem = styled.li`
-    border: 1px solid rgba(220, 227, 229, 0.8);
-    border-radius: 8px;
-    padding: 20px;
-    background: #fff;
+  border: 1px solid rgba(220, 227, 229, 0.8);
+  border-radius: 8px;
+  padding: 20px;
+  background: #fff;
 `;
