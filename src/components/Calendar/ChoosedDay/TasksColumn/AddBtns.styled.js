9b7fac9d2a03f9f6dd8cd7db5963cdee4768e51c
Yevgeniy Utkin <116801272+yevgeniy8@users.noreply.github.com>
2023-10-12 11:27:57 +0300

    commit

--- src/components/Calendar/ChoosedDay/TasksColumn/AddBtns.styled.js
+++ src/components/Calendar/ChoosedDay/TasksColumn/AddBtns.styled.js
@@ -1,73 +1,73 @@
 import styled from '@emotion/styled';
 import { tablet, mobile, desktop, min } from '../../../../styles/media';
 export const PlusIcon = styled.svg`
-  width: 100%;
-  height: 100%;
+    width: 100%;
+    height: 100%;
 
-  stroke: ${({ theme }) => theme.colors.textTitleHeader};
+    stroke: ${({ theme }) => theme.colors.textTitleHeader};
 `;
 export const TopBtn = styled.button`
-  display: flex;
+    display: flex;
 
-  width: 24px;
-  height: 24px;
-  padding: 0px;
+    width: 24px;
+    height: 24px;
+    padding: 0px;
 
-  border: none;
-  background: transparent;
-  align-items: center;
-  cursor: pointer;
+    border: none;
+    background: transparent;
+    align-items: center;
+    cursor: pointer;
 
-  stroke: ${({ theme }) => theme.colors.textTitleHeader};
+    stroke: ${({ theme }) => theme.colors.textTitleHeader};
 `;
 export const BtnToAdd = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
-  text-align: center;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 600;
-  line-height: 1.28;
-  border-radius: 8px;
-  border: 1px dashed #3e85f3;
+    text-align: center;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 1.28;
+    border-radius: 8px;
+    border: 1px dashed #3e85f3;
 
-  margin-top: 10px;
-  padding: 0px;
+    margin-top: 10px;
+    padding: 0px;
 
-  height: 48px;
-  width: 100%;
-
-  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
-
-  background: ${({ theme }) => theme.colors.backgroundSideBarCurrentPage};
-  color: ${({ theme }) => theme.colors.textPrimary};
-
-  &:hover {
-    background: ${({ theme }) => theme.colors.buttonAddTaskHover};
-  }
-
-  &:focus {
-    outline: none;
-    box-shadow: 0 0 4px #3e85f3;
-  }
-  ${min(mobile)} {
     height: 48px;
-  }
-  ${min(tablet)} {
-    height: 52px;
-  }
+    width: 100%;
 
-  ${min(desktop)} {
-    height: 52px;
-  }
+    transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1) 0s;
+
+    background: ${({ theme }) => theme.colors.backgroundSideBarCurrentPage};
+    color: ${({ theme }) => theme.colors.textPrimary};
+
+    &:hover {
+        background: ${({ theme }) => theme.colors.buttonAddTaskHover};
+    }
+
+    &:focus {
+        outline: none;
+        box-shadow: 0 0 4px #3e85f3;
+    }
+    ${min(mobile)} {
+        height: 48px;
+    }
+    ${min(tablet)} {
+        height: 52px;
+    }
+
+    ${min(desktop)} {
+        height: 52px;
+    }
 `;
 export const IconX = styled.svg`
-  width: 24px;
-  height: 24px;
+    width: 24px;
+    height: 24px;
 
-  margin-right: 8px;
+    margin-right: 8px;
 
-  stroke: ${({ theme }) => theme.colors.textTitleHeader};
+    stroke: ${({ theme }) => theme.colors.textTitleHeader};
 `;
--- src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
+++ src/components/Calendar/ChoosedDay/TasksColumn/ColumnsTasksList.styled.js
@@ -2,33 +2,34 @@ import styled from '@emotion/styled';
 import { tablet, mobile, desktop, min } from '../../../../styles/media';
 
 export const TaskList = styled.ul`
-  display: flex;
-  flex-direction: column;
-  position: relative;
-  gap: 17px;
+    display: flex;
+    flex-direction: column;
+    position: relative;
+    gap: 17px;
 
-  overflow: auto hidden;
-  overflow-y: auto;
+    /* overflow: auto hidden;
+    overflow-y: auto; */
 
-  padding-right: 7px;
-  margin-top: 24px;
-  height: 120px;
+    padding-right: 7px;
+    margin-top: 24px;
+    /* height: 120px; */
+    margin-bottom: 22px;
 
-  &::-webkit-scrollbar {
-    width: 8px;
-    padding: 50px;
-  }
+    &::-webkit-scrollbar {
+        width: 8px;
+        padding: 50px;
+    }
 
-  &::-webkit-scrollbar-thumb {
-    padding: 50px;
-    border-radius: 12px;
-    background: ${({ theme }) => theme.colors.scrollbar};
-  }
+    &::-webkit-scrollbar-thumb {
+        padding: 50px;
+        border-radius: 12px;
+        background: ${({ theme }) => theme.colors.scrollbar};
+    }
 
-  &::-webkit-scrollbar-track {
-    background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
-  }
-  ${min(mobile)} {
+    &::-webkit-scrollbar-track {
+        background-color: ${({ theme }) => theme.colors.backgroundScrollbar};
+    }
+    /* ${min(mobile)} {
     height: 120px;
   }
   ${min(tablet)} {
@@ -37,17 +38,17 @@ export const TaskList = styled.ul`
 
   ${min(desktop)} {
     height: 380px;
-  }
+  } */
 `;
 
 export const ColumnList = styled.ul`
-  display: flex;
-  gap: 27px;
+    display: flex;
+    gap: 27px;
 `;
 
 export const ColumnItem = styled.li`
-  border: 1px solid rgba(220, 227, 229, 0.8);
-  border-radius: 8px;
-  padding: 20px;
-  background: #fff;
+    border: 1px solid rgba(220, 227, 229, 0.8);
+    border-radius: 8px;
+    padding: 20px;
+    background: #fff;
 `;
